d13c44446a503d260b9ff8facfeaa609fe376e78
leongpet <leongj.peter@gmail.com>
2026-03-09 09:07:43 -0400

    Created magnitude.sv

--- verilog/src/magnitude.sv
+++ verilog/src/magnitude.sv
@@ -1,9 +1,18 @@
+import lockIn_pkg::*;
+
 module magnitude (
-    input   accumT  I,
-    input   accumT  Q,
-    output  accumT  magnitude
+    input  accumT      I,
+    input  accumT      Q,
+    output longAccumT  magOut
 );
 
-    
+    longAccumT I_sq;
+    longAccumT Q_sq;
+
+    always_comb begin
+        I_sq  = I * I;
+        Q_sq  = Q * Q;
+        magOut = I_sq + Q_sq;
+    end
 
 endmodule
